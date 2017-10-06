896475f123da355051c1a3569b9fcfc630a9b5a5
kevingilfether <kevingilfether@gmail.com>
2017-10-06 10:35:31 -0400

    Finished initial copy

--- Lucky_Numbers/Program.cs
+++ Lucky_Numbers/Program.cs
@@ -10,6 +10,18 @@ namespace Lucky_Numbers
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Let's play \"Guess the Number\"");
+            Console.WriteLine("I'm going to generate six (pseudo)random numbers between two that you tell me.");
+            Console.WriteLine("If you guess all the numbers right, you will win 1200 ducats!");
+            Console.WriteLine();
+
+            Console.WriteLine("What's the lower bound of the numbers you want to generate?");
+            int lowerBound = int.Parse(Console.ReadLine());
+            Console.WriteLine("And how about the highest limit?");
+            int upperBound = int.Parse(Console.ReadLine());
+
+
+
         }
     }
 }
